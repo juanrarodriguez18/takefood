20f98fc183b27ae90ebead7ab62d8a298f1c1fc8
juanrarodriguez18 <dragondr3w@gmail.com>
2018-09-14 11:06:51 +0200

    Commit Inicial con el esqueleto de la App

---
+++ takeAdvantageOfYourFood/manage.py
@@ -0,0 +1,31 @@
+# Copyright 2018 by TakeAdvantageOfFood contributors. All rights reserved.
+#
+# This file is part of TakeAdvantageOfFood.
+#
+#     TakeAdvantageOfFood is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     TakeAdvantageOfFood is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with TakeAdvantageOfFood.  If not, see <http:#www.gnu.org/licenses/>.
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'takeAdvantageOfYourFood.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
