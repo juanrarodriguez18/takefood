a347ebe167843ea5250bb66bba1c8ac8732fa908
juanrarodriguez18 <dragondr3w@gmail.com>
2018-09-18 22:24:11 +0200

    Añadida librería whitenoise

--- takeAdvantageOfYourFood/takeAdvantageOfYourFood/settings.py
+++ takeAdvantageOfYourFood/takeAdvantageOfYourFood/settings.py
@@ -58,12 +58,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'takeAdvantageOfYourFood.urls'
