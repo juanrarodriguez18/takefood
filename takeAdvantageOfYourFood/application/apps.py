cdc4fbce63e0aa811dc0314f7738520d3a4ff4b8
juanrarodriguez18 <dragondr3w@gmail.com>
2018-09-22 22:53:09 +0200

    Añadidos Copyright en los archivos en los que no se había incluido

--- takeAdvantageOfYourFood/application/apps.py
+++ takeAdvantageOfYourFood/application/apps.py
@@ -1,3 +1,19 @@
+# Copyright 2018 by TakeAdvantageOfFood contributors. All rights reserved.
+#
+# This file is part of TakeAdvantageOfFood.
+#
+#     TakeAdvantageOfFood is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     TakeAdvantageOfFood is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with TakeAdvantageOfFood.  If not, see <http:#www.gnu.org/licenses/>.
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
